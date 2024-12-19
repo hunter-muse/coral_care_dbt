532ffc82554cf025cc4aa5d150e606727f7dac9a
Hunter <hunter@gooddata.studio>
2024-12-19 15:41:16 -0500

    claims data add

--- models/intermediate/int__parent_dependent_crosswalk.sql
+++ models/intermediate/int__parent_dependent_crosswalk.sql
@@ -4,13 +4,15 @@ parent.parent_first_name,
 parent.parent_last_name,
 dependent.dependent_id,
 dependent.dependent_name,
+dependent.dependent_first_name,
+dependent.dependent_last_name,
 dependent.dependent_dob,
 dependent.dependent_gender
 from 
 {{ref('int__parent')}} parent 
 LEFT JOIN 
 {{ref('int__dependent')}} dependent
-on parent.parent_id = dependent.parent_id
+on parent.parent_user_reference = dependent.parent_id
 where dependent.dependent_name is not null
 order by parent.parent_id
 

ddbb303d2e7675c54b003772120437a0adfb498c
Hunter <hunter@gooddata.studio>
2025-01-09 18:19:50 -0500

    added a few fields - 1,5,25 completed session for providers

--- models/intermediate/int__session_provider.sql
+++ models/intermediate/int__session_provider.sql
@@ -14,21 +14,31 @@ LEFT JOIN {{ref('int__provider')}} AS provider
 ON session.provider_id = provider.provider_id
 ),
 
+session_numbers as (
+    select 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY provider_id ORDER BY session_start_date) as session_number
+    from provider_summary
+    where session_status = 'Completed'
+),
+
+milestone_sessions as (
+    select 
+        provider_id,
+        MAX(CASE WHEN session_number = 1 THEN session_start_date END) as first_session_complete_ts,
+        MAX(CASE WHEN session_number = 5 THEN session_start_date END) as fifth_session_complete_ts,
+        MAX(CASE WHEN session_number = 25 THEN session_start_date END) as twenty_fifth_session_complete_ts
+    from session_numbers
+    group by provider_id
+),
+
 final as (
 select 
 provider_id, 
     MIN(CASE WHEN rs.session_status IN ('Completed') THEN rs.session_start_date END) AS first_session_date,
-
-    -- Most recent session details
     MAX(CASE WHEN rs.session_status = 'Completed' AND  rs.session_start_date < CURRENT_DATE() THEN rs.session_start_date ELSE NULL END) as most_recent_session_date,
-
-    -- Upcoming session details
     MIN(CASE WHEN rs.session_status = 'Confirmed' AND  rs.session_start_date >= CURRENT_DATE() THEN rs.session_start_date ELSE NULL END) as upcoming_session_date,
-    
-    -- Farthest out session details
     MAX(CASE WHEN rs.session_status = 'Confirmed' AND  rs.session_start_date >= CURRENT_DATE() THEN rs.session_start_date ELSE NULL END) as farthest_out_session_date,
-    
-    -- Other aggregates
     COUNT(CASE WHEN rs.session_status IN ('Completed', 'Confirmed') THEN 1 ELSE NULL END) as total_completed_session_count,
     COUNT(CASE WHEN rs.session_status IN ('Completed', 'Confirmed') AND  days_since_session > -30 THEN 1 ELSE NULL END) as Completed_Last_30_Days,
     COUNT(DISTINCT CASE WHEN rs.session_status IN ('Completed', 'Confirmed') AND  days_since_session > -30 THEN dependent_id ELSE NULL END) as Unique_Patient_Count_Last_30_Days
@@ -37,12 +47,16 @@ GROUP BY 1
 )
 
 select 
-    provider_id,
-    total_completed_session_count,
-    Completed_Last_30_Days,
-    Unique_Patient_Count_Last_30_Days,
-    CAST(first_session_date AS DATE) as first_session_date,
-    CAST(most_recent_session_date AS DATE) as most_recent_session_date,
-    CAST(upcoming_session_date AS DATE) as upcoming_session_date,
-    CAST(farthest_out_session_date AS DATE) as farthest_out_session_date
-from final
+    f.provider_id,
+    f.total_completed_session_count,
+    f.Completed_Last_30_Days,
+    f.Unique_Patient_Count_Last_30_Days,
+    CAST(f.first_session_date AS DATE) as first_session_date,
+    CAST(f.most_recent_session_date AS DATE) as most_recent_session_date,
+    CAST(f.upcoming_session_date AS DATE) as upcoming_session_date,
+    CAST(f.farthest_out_session_date AS DATE) as farthest_out_session_date,
+    CAST(m.first_session_complete_ts AS DATE) as first_session_complete_ts,
+    CAST(m.fifth_session_complete_ts AS DATE) as fifth_session_complete_ts,
+    CAST(m.twenty_fifth_session_complete_ts AS DATE) as twenty_fifth_session_complete_ts
+from final f
+LEFT JOIN milestone_sessions m ON f.provider_id = m.provider_id
